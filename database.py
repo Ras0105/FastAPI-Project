8e6a6bb8d68ed5b1e646aa23c3a7ac2cb7630e03
Ras0105 <smt29nov@gmail.com>
2026-06-30 23:16:40 +0530

    minor commit

--- database.py
+++ database.py
@@ -1,5 +1,7 @@
 from pymongo import MongoClient
 client=MongoClient("mongodb://localhost:27017/")
+# MongoDB Atlas
+# client=MongoClient("mongodb+srv://Ras0105:<db_password>@cluster0.nf7ib5e.mongodb.net/?appName=Cluster0")
 db=client["University_DB"]
 students_collection=db["Students"]
 print("MongoDB Connected Successfully!")
